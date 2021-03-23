bb20b8eaf938731776d55074deaa6c4e439c8dde
Kris Litman <kris.d.litman@gmail.com>
2021-03-23 15:58:54 -0400

    User can register, test made as well
    Co-authored-by: Tommy Nieuwenhuis <72888891+tsnieuwen@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true
-
-  has_secure_password
 end
--- spec/features/users/registering_users_spec.rb
+++ spec/features/users/registering_users_spec.rb
@@ -1,21 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "User registration form" do
-  it "creates new user" do
-    visit root_path 
+RSpec.describe 'User registration form' do
+  scenario 'it creates a new user' do
+    visit root_path
 
-    click_on "Register as a User"
+    click_link 'Register as a User'
 
-    expect(current_path).to eq("/users/new")
+    fill_in 'user[email]', with: 'someone@example.com'
+    fill_in 'user[password]', with: 'password1234'
 
-    email = "funbucket13"
-    password = "test"
+    click_button 'Register User'
 
-    fill_in :email, with: email
-    fill_in :password, with: password
-
-    click_on "Register User"
-
-    expect(page).to have_content("Welcome, #{email}!")
+    expect(page).to have_content('Welcome someone@example.com!')
   end
 end
