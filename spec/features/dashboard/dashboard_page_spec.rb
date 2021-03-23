e222d82ecf42cf9dde18438fe730431ec0ed998b
Kris Litman <kris.d.litman@gmail.com>
2021-03-23 19:20:10 -0400

    Added dashboard test, and to index page

--- spec/features/dashboard/dashboard_page_spec.rb
+++ spec/features/dashboard/dashboard_page_spec.rb
@@ -2,22 +2,56 @@ require 'rails_helper'
 
 RSpec.describe "As an authenticated user" do
   describe "When I visit '/dashboard'" do
-    before :each do
-      @user = User.create(
+    it "I don't see ~Welcome <username> at top of page when not a user" do
+      visit root_path
+
+      expect(page).not_to have_link("Dashboard")
+    end
+
+    it "Has link at top of the page" do
+      user = User.create(
         email: 'user@example.com',
         password: 'test'
       )
-    end
-    it "I see ~Welcome <username> at top of page" do
+
       visit root_path
+
+      click_link('I already have an account')
+      fill_in :email, with: user.email
+      fill_in :password, with: 'test'
+      click_button 'Log In'
+
+      expect(page).to have_link('Dashboard')
 
       click_link 'Dashboard'
 
       expect(current_path).to eq(dashboard_path)
     end
 
-    it "Has button to discover movies"
-    it "Has section for friends"
-    it "Has a viewing parties section"
+    it "Has button to discover movies, section for friends and parties" do 
+      user = User.create(
+        email: 'user@example.com',
+        password: 'test'
+      )
+
+      visit root_path
+
+      click_link('I already have an account')
+      fill_in :email, with: user.email
+      fill_in :password, with: 'test'
+      click_button 'Log In'
+      click_link 'Dashboard'
+
+      expect(page).to have_content("Welcome #{user.email}!")
+      expect(page).to have_button('Discover Movies')
+      
+      within('.friends') do
+        expect(page).to have_content('Friends')
+      end
+
+      within('.parties') do
+        expect(page).to have_content('Viewing Parties')
+      end
+    end
   end
 end
