1891f4ab54ad13adbb40a1f7ba052453f672b4f0
Kris Litman <kris.d.litman@gmail.com>
2021-03-30 17:02:25 -0400

    Made movie service test

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -4,38 +4,38 @@ RSpec.describe MovieFacade do
   describe "Methods" do
     it "#top_rated_movies" do
       VCR.use_cassette('movie_facade_test1') do
-      movies = MovieFacade.top_rated_movies
+        movies = MovieFacade.top_rated_movies
 
-      expect(movies).to be_an(Array)
-      expect(movies.length).to eq 40
+        expect(movies).to be_an(Array)
+        expect(movies.length).to eq 40
       end
     end
     it "#search" do
       VCR.use_cassette('movie_facade_test2') do
-      input = "Lord of the Rings"
-      movies = MovieFacade.search(input)
+        input = "Lord of the Rings"
+        movies = MovieFacade.search(input)
       
-      expect(movies).to be_an(Array)
-      expect(movies.first).to be_an_instance_of MatchMovie
+        expect(movies).to be_an(Array)
+        expect(movies.first).to be_an_instance_of MatchMovie
       end
     end
     it "#show_movie" do
       VCR.use_cassette('movie_facade_test3') do
-      input = "123"
-      movie = MovieFacade.show_movie(input)
+        input = "123"
+        movie = MovieFacade.show_movie(input)
 
-      expect(movie).to be_an_instance_of ShowMovie
-      expect(movie.title).to eq("The Lord of the Rings")
+        expect(movie).to be_an_instance_of ShowMovie
+        expect(movie.title).to eq("The Lord of the Rings")
       end
     end
     it "#get_details" do
       VCR.use_cassette('movie_facade_test4') do
-      input = "100"
-      details = MovieFacade.get_details(input)
-      
-      expect(details).to be_a(Hash)
-      expect(details[:all_reviews].length).to eq 3
-      expect(details[:all_cast].length).to eq 30
+        input = "100"
+        details = MovieFacade.get_details(input)
+
+        expect(details).to be_a(Hash)
+        expect(details[:all_reviews].length).to eq 3
+        expect(details[:all_cast].length).to eq 30
       end
     end
   end
---
+++ spec/services/movie_service_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe MovieService do
+  describe "Methods" do
+    it "top_rated_movies" do
+      VCR.use_cassette('movie_service_test1') do
+        movies = MovieService.top_rated_movies
+  
+        expect(movies).to be_an(Array)
+        expect(movies.length).to eq 40
+      end
+    end
+    it "search" do
+      VCR.use_cassette('movie_service_test2') do
+        input = "Lord of the Rings"
+        movies = MovieService.matched_movies(input)
+        
+        expect(movies).to be_an(Array)
+        expect(movies.first).to be_an_instance_of MatchMovie
+        expect(movies.length).not_to eq 40
+      end
+    end
+    it "reviews" do
+      VCR.use_cassette('movie_service_test3') do
+        input = "100"
+        details = MovieService.reviews(input)
+
+        expect(details).to be_a(Hash)
+        expect(details[:all_reviews].length).to eq 3
+        expect(details[:all_cast].length).to eq 30
+      end
+    end
+  end
+end
